6c42e271bf5b2b239d9dad5afd7e6d727b421300
TahaRKD <taharokadiya@gmail.com>
2026-08-08 23:01:08 +0530

    first upload

---
+++ game.py
@@ -0,0 +1,56 @@
+# Steps:
+# 1)printing the game board
+# 2)take player input
+# 3) check for win or tie
+# 4) switch the player 
+# 5) check for win or tie again 
+
+import random
+from itertools import permutations
+def printBoard(board):
+    x=0
+    for i in range(0,9,3):
+        x+=1
+        print(" |".join(board[i:i+3]))
+        if x!=3:
+            print("__|__|__")
+
+def checkWin(player):
+    global winner
+    winCombs=[[0,4,8],[6,4,2],[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8]]
+    for i,j,k in permutations(player,3):
+        if [i,j,k] in winCombs:
+            print("someone won!")
+            winner=True
+            break
+def playerInput():
+    global playerInputs
+    global board
+    no=int(input("select the position:"))
+    board[no]=s
+    playerInputs.append(no)
+    turn.remove(no)
+def compPlay():
+    choice=random.choice(turn)
+    turn.remove(choice)
+    compInputs.append(choice)
+    if s=='X':
+        board[choice]="O"
+    else:
+        board[choice]="X"
+
+winner=False
+s=str(input("you want X or O:"))
+board=["0","1","2","3","4","5","6","7","8"]
+turn=[0,1,2,3,4,5,6,7,8]
+currentPlayer="X"
+winner=False
+gameRunning= True
+playerInputs=[]
+compInputs=[]
+while winner==False:
+    printBoard(board)
+    playerInput()
+    checkWin(playerInputs)
+    compPlay()
+    checkWin(compInputs)
